c99a9a4ed4800b9e910477b5d481d197cb3a3198
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-06-25 16:25:03 -0700

    save

---
+++ template.cu
@@ -0,0 +1,25 @@
+template<long long i>
+__global__ void add_kernel(float *a, float *b, float *c, int N) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < N) {
+        c[idx] = a[idx] + b[idx];
+    }
+}
+
+template<long long l = 0, long long r = 65536>
+void _add(float *a, float *b, float *c, int N) {
+    if constexpr (l == r) {
+        add_kernel<l><<<(N + 255) / 256, 256>>>(a, b, c, N);
+    } else if constexpr (l + 1 == r) {
+        add_kernel<l><<<(N + 255) / 256, 256>>>(a, b, c, N);
+        add_kernel<r><<<(N + 255) / 256, 256>>>(a, b, c, N);
+    } else {
+        constexpr long long m = (l + r) / 2;
+        _add<l, m>(a, b, c, N);
+        _add<m, r>(a, b, c, N);
+    }
+}
+
+void add(float *a, float *b, float *c, int N) {
+    _add(a, b, c, N);
+}
